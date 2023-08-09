2609e79cb6d5d24ee67f046069599fbeb2ade929
melkamu <melkemk503@gmail.com>
2023-08-09 12:32:00 +0300

    inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,63 +1,22 @@
-''' An empty class representing the base geometry.'''
-
-
-class BaseMetaClass(type):
-    """
-    overrides.
-    """
-
-    def __dir__(cls):
-        return [
-            attribute
-            for attribute in super().__dir__()
-            if attribute != '__init_subclass__'
-        ]
-
-
-class BaseGeometry(metaclass=BaseMetaClass):
-    """
-    Do nothing: By passing pass.
-    """
-
-    def __dir__(cls):
-        return [
-            attribute
-            for attribute in super().__dir__()
-            if attribute != '__init_subclass__'
-        ]
+class BaseGeometry:
+    """ Base geometry class """
 
     def area(self):
-        '''
-        Public instance method that raises an Exception
-        '''
-        raise Exception('area() is not implemented')
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        '''
-        Public instance method that validate a value
-        Attr:
-            name(string): the name string.
-            value(int): must be an integer greater than 0.
-        '''
-        self.name = name
-        self.value = value
-
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-
+        """ Validate the value """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
-
-'''Rectangle class that inherit from BaseGeometry'''
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-
-    '''Initializing with and height'''
+    """ Rectangle class """
 
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
