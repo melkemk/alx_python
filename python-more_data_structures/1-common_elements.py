e40dbf36dec77117fb621cb738672d7da152e0e8
melkamu <melkemk503@gmail.com>
2023-08-09 12:06:43 +0300

    first

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,12 @@
+def common_elements(set_1, set_2):
+    common_set = set()
+    for element in set_1:
+        if element in set_2:
+            common_set.add(element)
+    return common_set
+
+# Test the function with the given example
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,5 @@
+def best_score(a_dictionary):
+    if a_dictionary:
+        best_key = max(a_dictionary, key=a_dictionary.get)
+        return best_key
+    return None
