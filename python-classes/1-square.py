14103c83d5341525a5aac7ce4369bece17cf9d38
melkamu <melkemk503@gmail.com>
2023-08-09 12:18:30 +0300

    class

---
+++ python-classes/1-square.py
@@ -0,0 +1,23 @@
+""" 
+defining a class to get the area of square
+ """
+
+class Square:
+    """
+    Represents a square with a private size attribute.
+    """
+
+    def __init__(self, size=0):
+
+        """
+        Initializes a Square instance with the provided size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size
