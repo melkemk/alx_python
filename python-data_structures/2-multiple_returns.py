83b0a86cb96543f3551a985058b9a58c44341762
melkamu <melkemk503@gmail.com>
2023-08-09 11:54:45 +0300

    dsa

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -2,4 +2,4 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else None
-    return "Length: {:d} - First character: {}".format(length, first_char)
+    return length, first_char
