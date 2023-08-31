70141589eb7238b6c1c67c134a37903013f75861
melkamu <melkemk503@gmail.com>
2023-08-31 15:56:31 +0300

    fd

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -18,6 +18,17 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return f'C {text.replace("_", " ")}'
+    
+@app.route('/python/<text>', strict_slashes=False)
+def c_text(text):
+    if text:
+        return f'Python {text.replace("_", " ")}'
+    else:
+        return 'Python cool'
+@app.route('/number/<n:int>', strict_slashes=False)
+def c_text(n):
+    if n==int(n):
+        return f' {n} is number'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
